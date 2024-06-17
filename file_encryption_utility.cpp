5844a080c3031b0fcbaf5d5a467f4294796eb529
Aneesh Sharma <aneeshsharma1024@gmail.com>
2024-06-17 16:11:50 +0530

    Final Test

--- file_encryption_utility.cpp
+++ file_encryption_utility.cpp
@@ -1,49 +1,49 @@
 #ifndef UNICODE
 #define UNICODE
 #endif
-
 #include <windows.h>
-#include <iostream>
 #include <commdlg.h>
+#include <string>
 #include <fstream>
 #include <vector>
 #include <wincrypt.h>
+#include <iostream>
 
-#define BUTTON_ID 1
+#pragma comment(lib, "advapi32.lib")
 
-// Function Prototype's
-LRESULT CALLBACK WinProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
-std::string ConvertWideStringToNarrowString(const std::wstring &widestring);
-bool EncryptData(const std::vector<BYTE> &data, std::vector<BYTE> &encryptedData);
-void ReadFileToVector(const std::string &filename, std::vector<BYTE> &buffer);
-void WriteVectorToFile(const std::string &filename, std::vector<BYTE> &buffer);
-void showOpenFileDialog(HWND hwnd);
+#define IDC_FILE_EDIT 101
+#define IDC_KEY_EDIT 102
+#define IDC_BROWSE_BUTTON 103
+#define IDC_ENCRYPT_BUTTON 104
+#define IDC_DECRYPT_BUTTON 105
 
-// WinAPI Initialization
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+void AddControls(HWND);
+void BrowseFile(HWND);
+bool EncryptFileX(const std::wstring &, const std::wstring &);
+bool DecryptFile(const std::wstring &, const std::wstring &);
+
+std::string wstringToString(const std::wstring &wstr)
 {
-    const wchar_t *CLASS_NAME = L"File Encrypt Window Class";
-    const wchar_t *WINDOW_NAME = L"File Encryption Utility";
+    int bufferSize = WideCharToMultiByte(CP_UTF8, 0, wstr.c_str(), -1, NULL, 0, NULL, NULL);
+    std::string str(bufferSize, 0);
+    WideCharToMultiByte(CP_UTF8, 0, wstr.c_str(), -1, &str[0], bufferSize, NULL, NULL);
+    return str;
+}
 
-    WNDCLASS wc = {};
-    wc.lpfnWndProc = WinProc;
-    wc.hInstance = hInstance;
-    wc.lpszClassName = CLASS_NAME;
-    wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR args, int ncmdshow)
+{
+    WNDCLASSW wc = {0};
+    wc.hbrBackground = (HBRUSH)COLOR_WINDOW;
+    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wc.hInstance = hInst;
+    wc.lpszClassName = L"FileEncryptor";
+    wc.lpfnWndProc = WndProc;
 
-    if (!RegisterClass(&wc))
-    {
+    if (!RegisterClassW(&wc))
         return -1;
-    }
 
-    HWND hWnd = CreateWindowEx(0, CLASS_NAME, WINDOW_NAME, WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, 400, 400, NULL, NULL, hInstance, NULL);
-    if (!hWnd)
-    {
-        return -1;
-    }
-
-    ShowWindow(hWnd, nCmdShow);
-    UpdateWindow(hWnd);
+    CreateWindowW(L"FileEncryptor", L"File Encryption Tool", WS_OVERLAPPEDWINDOW | WS_VISIBLE, 100, 100, 500, 200, NULL, NULL, NULL, NULL);
 
     MSG msg = {0};
     while (GetMessage(&msg, NULL, 0, 0))
@@ -51,178 +51,193 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
         TranslateMessage(&msg);
         DispatchMessage(&msg);
     }
-
-    return (int)msg.wParam;
+    return 0;
 }
 
-LRESULT CALLBACK WinProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp)
 {
-    switch (uMsg)
+    switch (msg)
     {
-    case WM_CREATE:
-    {
-        HWND createButton = CreateWindowEx(0, L"Button", L"Select File", WS_TABSTOP | WS_CHILD | WS_VISIBLE | BS_DEFPUSHBUTTON, 150, 150, 100, 40, hwnd, (HMENU)BUTTON_ID, (HINSTANCE)GetWindowLongPtr(hwnd, GWLP_HINSTANCE), NULL);
-        if (!createButton)
-        {
-            std::cout << "Error in creating Button :" << GetLastError();
-            return -1;
-        }
-        break;
-    }
-
     case WM_COMMAND:
-        if (LOWORD(wParam) == BUTTON_ID)
+        switch (wp)
         {
-            // MessageBox(hwnd, L"Button Clicked", L"Notification", MB_ICONINFORMATION);
-            showOpenFileDialog(hwnd);
+        case IDC_BROWSE_BUTTON:
+            BrowseFile(hwnd);
+            break;
+        case IDC_ENCRYPT_BUTTON:
+        {
+            wchar_t filePath[256], key[256];
+            GetDlgItemText(hwnd, IDC_FILE_EDIT, filePath, 256);
+            GetDlgItemText(hwnd, IDC_KEY_EDIT, key, 256);
+            if (EncryptFileX(filePath, key))
+            {
+                MessageBox(hwnd, L"File Encrypted Successfully", L"Success", MB_OK);
+            }
+            else
+            {
+                std::cout << GetLastError();
+                MessageBox(hwnd, L"Encryption Failed", L"Error", MB_OK);
+            }
+            break;
+        }
+        case IDC_DECRYPT_BUTTON:
+        {
+            wchar_t filePath[256], key[256];
+            GetDlgItemText(hwnd, IDC_FILE_EDIT, filePath, 256);
+            GetDlgItemText(hwnd, IDC_KEY_EDIT, key, 256);
+            if (DecryptFile(filePath, key))
+            {
+                std::cout << GetLastError();
+                MessageBox(hwnd, L"File Decrypted Successfully", L"Success", MB_OK);
+            }
+            else
+            {
+                MessageBox(hwnd, L"Decryption Failed", L"Error", MB_OK);
+            }
+            break;
+        }
         }
         break;
-
+    case WM_CREATE:
+        AddControls(hwnd);
+        break;
     case WM_DESTROY:
         PostQuitMessage(0);
-        return 0;
-
-    case WM_PAINT:
-    {
-        PAINTSTRUCT ps;
-        HDC hdc = BeginPaint(hwnd, &ps);
-        FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
-        EndPaint(hwnd, &ps);
+        break;
+    default:
+        return DefWindowProc(hwnd, msg, wp, lp);
     }
-        return 0;
-    }
-
-    return DefWindowProc(hwnd, uMsg, wParam, lParam);
+    return 0;
 }
 
-void showOpenFileDialog(HWND hwnd)
+void AddControls(HWND hwnd)
+{
+    CreateWindowW(L"static", L"File:", WS_VISIBLE | WS_CHILD, 20, 20, 50, 20, hwnd, NULL, NULL, NULL);
+    CreateWindowW(L"edit", NULL, WS_VISIBLE | WS_CHILD | WS_BORDER | ES_AUTOHSCROLL, 70, 20, 300, 20, hwnd, (HMENU)IDC_FILE_EDIT, NULL, NULL);
+    CreateWindowW(L"button", L"Browse", WS_VISIBLE | WS_CHILD, 380, 20, 80, 20, hwnd, (HMENU)IDC_BROWSE_BUTTON, NULL, NULL);
+
+    CreateWindowW(L"static", L"Key:", WS_VISIBLE | WS_CHILD, 20, 60, 50, 20, hwnd, NULL, NULL, NULL);
+    CreateWindowW(L"edit", NULL, WS_VISIBLE | WS_CHILD | WS_BORDER | ES_AUTOHSCROLL, 70, 60, 300, 20, hwnd, (HMENU)IDC_KEY_EDIT, NULL, NULL);
+
+    CreateWindowW(L"button", L"Encrypt", WS_VISIBLE | WS_CHILD, 100, 100, 100, 30, hwnd, (HMENU)IDC_ENCRYPT_BUTTON, NULL, NULL);
+    CreateWindowW(L"button", L"Decrypt", WS_VISIBLE | WS_CHILD, 220, 100, 100, 30, hwnd, (HMENU)IDC_DECRYPT_BUTTON, NULL, NULL);
+}
+
+void BrowseFile(HWND hwnd)
 {
     OPENFILENAME ofn;
-    wchar_t szFile[260] = {0};
-
-    // // Initialise OPENFILENAME
-    ZeroMemory(&ofn, sizeof(ofn));
-    ofn.lStructSize = sizeof(ofn);
+    wchar_t file_name[100] = {0};
+    ZeroMemory(&ofn, sizeof(OPENFILENAME));
+    ofn.lStructSize = sizeof(OPENFILENAME);
     ofn.hwndOwner = hwnd;
-    ofn.lpstrFile = szFile;
-    ofn.lpstrFile[0] = '\0';
-    ofn.nMaxFile = sizeof(szFile) / sizeof(szFile[0]);
-    ofn.lpstrFilter = L"ALL\0*.*\0Text\0*.TXT\0";
+    ofn.lpstrFile = file_name;
+    ofn.nMaxFile = sizeof(file_name) / sizeof(wchar_t);
+    ofn.lpstrFilter = L"All Files\0*.*\0";
     ofn.nFilterIndex = 1;
-    ofn.lpstrFileTitle = NULL;
-    ofn.nMaxFileTitle = 0;
-    ofn.lpstrInitialDir = NULL;
-    ofn.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST;
 
-    if (GetOpenFileName(&ofn) == TRUE)
-    {
-        std::string fileName = "";
-        std::wstring filePath = ofn.lpstrFile;
-
-        int length = wcslen(ofn.lpstrFile);
-
-        for (int i = length - 1; i >= 0 && ofn.lpstrFile[i] != '\\'; i--)
-        {
-            fileName = (char)ofn.lpstrFile[i] + fileName;
-        }
-        std::cout << "\n"
-                  << "Selected File: " << fileName << std::endl;
-        std::wcout
-            << L"File Location : " << ofn.lpstrFile << std::endl;
-
-        std::string narrowFilePath(filePath.begin(), filePath.end());
-
-        std::vector<BYTE> data;
-        ReadFileToVector(narrowFilePath, data);
-
-        // Encrypt the data
-        std::vector<BYTE> encryptedData;
-        if (EncryptData(data, encryptedData))
-        {
-            std::string encryptedFilePath = narrowFilePath + ".enc";
-            WriteVectorToFile(encryptedFilePath, encryptedData);
-
-            std::wcout << L"Selected File: " << filePath << std::endl;
-            std::cout << "Encrypted File: " << encryptedFilePath << std::endl;
-        }
-        else
-        {
-            std::cerr << "Encryption Failed. " << std::endl;
-        }
-    }
-    else
-    {
-        DWORD error = CommDlgExtendedError();
-        if (error != 0)
-        {
-            std::wcerr << L"Failed to open file Dailog. Error Code: " << error << std::endl;
-        }
-    }
+    GetOpenFileName(&ofn);
+    SetDlgItemText(hwnd, IDC_FILE_EDIT, ofn.lpstrFile);
 }
 
-std::string ConvertWideStringToNarrowString(const std::wstring &widestring)
+bool EncryptDecryptFile(const std::wstring &filePath, const std::wstring &key, bool encrypt)
 {
-    int bufferSize = WideCharToMultiByte(CP_UTF8, 0, widestring.c_str(), -1, NULL, 0, NULL, NULL);
-    std::vector<char> buffer(bufferSize);
-    WideCharToMultiByte(CP_UTF8, 0, widestring.c_str(), -1, buffer.data(), bufferSize, NULL, NULL);
-    return std::string(buffer.data());
-}
-
-bool EncryptData(const std::vector<BYTE> &data, std::vector<BYTE> &encryptedData)
-{
-    HCRYPTPROV hProv = 0;
-    HCRYPTKEY hKey = 0;
-    HCRYPTHASH hHash = 0;
+    HCRYPTPROV hProv;
+    HCRYPTKEY hKey;
+    HCRYPTHASH hHash;
 
     if (!CryptAcquireContext(&hProv, NULL, NULL, PROV_RSA_AES, CRYPT_VERIFYCONTEXT))
     {
-        std::cerr << "CryptAcquiredContext Failed." << std::endl;
         return false;
     }
 
-    if (!CryptCreateHash(hProv, CALG_SHA, 0, 0, &hHash))
+    if (!CryptCreateHash(hProv, CALG_SHA1, 0, 0, &hHash))
     {
-        std::cerr << "CryptCreateHash failed. " << std::endl;
         CryptReleaseContext(hProv, 0);
         return false;
     }
 
-    // Using PASSWORD to hash data
-
-    const BYTE *password = reinterpret_cast<const BYTE *>("password");
-    DWORD passwordLen = strlen("password");
-
-    // if(!CryptHashData(hHash, password, passwordLen,0)){}
+    std::string keyStr = wstringToString(key);
+    if (!CryptHashData(hHash, (BYTE *)keyStr.c_str(), keyStr.length(), 0))
+    {
+        CryptDestroyHash(hHash);
+        CryptReleaseContext(hProv, 0);
+        return false;
+    }
 
     if (!CryptDeriveKey(hProv, CALG_AES_256, hHash, 0, &hKey))
     {
-        std::cerr << "CryptDeriveKey failed. " << std::endl;
         CryptDestroyHash(hHash);
         CryptReleaseContext(hProv, 0);
         return false;
     }
 
-    encryptedData = data;
-    DWORD dataLen = encryptedData.size();
+    CryptDestroyHash(hHash);
 
-    if (!CryptEncrypt(hKey, NULL, TRUE, 0, encryptedData.data(), &dataLen, encryptedData.size()))
+    HANDLE hSourceFile = CreateFile(filePath.c_str(), GENERIC_READ, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+    if (hSourceFile == INVALID_HANDLE_VALUE)
     {
-        std::cerr << "CryptEncrypt failed. " << std::endl;
         CryptDestroyKey(hKey);
-        CryptDestroyHash(hHash);
         CryptReleaseContext(hProv, 0);
         return false;
     }
+
+    std::wstring outFilepath = encrypt ? (filePath + L".enc") : (filePath + L".dec");
+    HANDLE hDestFile = CreateFile(outFilepath.c_str(), GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+    if (hDestFile == INVALID_HANDLE_VALUE)
+    {
+        CloseHandle(hSourceFile);
+        CryptDestroyKey(hKey);
+        CryptReleaseContext(hProv, 0);
+        return false;
+    }
+
+    const DWORD bufferSize = 4096;
+    BYTE buffer[bufferSize];
+    BOOL result;
+    DWORD bytesRead, bytesWritten;
+    while (ReadFile(hSourceFile, buffer, bufferSize, &bytesRead, NULL) && bytesRead > 0)
+    {
+        if (encrypt)
+        {
+            result = CryptEncrypt(hKey, 0, bytesRead < bufferSize, 0, buffer, &bytesRead, bufferSize);
+        }
+        else
+        {
+            result = CryptDecrypt(hKey, 0, bytesRead < bufferSize, 0, buffer, &bytesRead);
+        }
+
+        if (!result)
+        {
+            CloseHandle(hSourceFile);
+            CloseHandle(hDestFile);
+            CryptDestroyKey(hKey);
+            CryptReleaseContext(hProv, 0);
+            return false;
+        }
+
+        if (!WriteFile(hDestFile, buffer, bytesRead, &bytesWritten, NULL))
+        {
+            CloseHandle(hSourceFile);
+            CloseHandle(hDestFile);
+            CryptDestroyKey(hKey);
+            CryptReleaseContext(hProv, 0);
+            return false;
+        }
+    }
+
+    CloseHandle(hSourceFile);
+    CloseHandle(hDestFile);
+    CryptDestroyKey(hKey);
+    CryptReleaseContext(hProv, 0);
+    return true;
 }
 
-void ReadFileToVector(const std::string &filename, std::vector<BYTE> &buffer)
+bool EncryptFileX(const std::wstring &filePath, const std::wstring &key)
 {
-    std::ifstream file(filename, std::ios::binary);
-    buffer = std::vector<BYTE>(std::istreambuf_iterator<char>(file), {});
+    return EncryptDecryptFile(filePath, key, true);
 }
 
-void WriteVectorToFile(const std::string &filename, std::vector<BYTE> &buffer)
+bool DecryptFile(const std::wstring &filePath, const std::wstring &key)
 {
-    std::ofstream file(filename, std::ios::binary);
-    file.write(reinterpret_cast<const char *>(buffer.data()), buffer.size());
+    return EncryptDecryptFile(filePath, key, false);
 }
